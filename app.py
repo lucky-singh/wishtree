701c6ba70aaaa3228a7ad5b6bf6d5e52fd232fad
lucky-singh <luckysingh223@gmail.com>
2018-11-23 18:48:58 +0530

    Change Postgresql to sqlite for easy setup

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
- 
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request, url_for, redirect
 from models import *
 from sqlalchemy.sql.expression import func
-
-from os import getenv
+from datetime import date, datetime
 
 app = Flask(__name__)
 
 # Database connection
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///wishtree.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -22,13 +21,13 @@ def main():
     # Initialize with dummy data
     if Feature.query.first() == None:
         f1 = Feature(title="Flannery O'Connor, Mystery and Manners: Occasional Prose", description="Anybody who has survived his childhood has enough information about life to last him the rest of his days.",
-                     client="Client A", priority=1, targetDate="01-12-2018", productArea="Policies")
-        f2 = Feature(title="Carlos Ruiz Zafón, The Shadow of the Wind", description="A story is a letter that the author writes to himself, to tell himself things that he would be unable to discover otherwise.",
-                     client="Client A", priority=2, targetDate="02-12-2018", productArea="Billing")
+                     client="Client A", priority=1, targetDate=date(2018, 12, 1), productArea="Policies")
+        f2 = Feature(title="Carlos Ruiz Zafon, The Shadow of the Wind", description="A story is a letter that the author writes to himself, to tell himself things that he would be unable to discover otherwise.",
+                     client="Client A", priority=2, targetDate=date(2018, 12, 2), productArea="Billing")
         f3 = Feature(title="Thomas Mann, Essays of Three Decades", description="A writer is someone for whom writing is more difficult than it is for other people.",
-                     client="Client B", priority=1, targetDate="01-12-2018", productArea="Claims")
+                     client="Client B", priority=1, targetDate=date(2018, 12, 1), productArea="Claims")
         f4 = Feature(title="J.D. Salinger, The Catcher in the Rye", description="What really knocks me out is a book that, when you're all done reading it, you wish the author that wrote it was a terrific friend of yours and you could call him up on the phone whenever you felt like it. That doesn't happen much, though.",
-                     client="Client c", priority=1, targetDate="03-12-2018", productArea="Reports")
+                     client="Client c", priority=1, targetDate=date(2018, 12, 3), productArea="Reports")
 
         db.session.add(f1)
         db.session.add(f2)
@@ -77,12 +76,11 @@ def index():
         featureDescription = request.form.get("featureDescription")
         featureClient = request.form.get("featureClient")
         featureClientPriority = request.form.get("featureClientPriority")
-        featureTargetDate = request.form.get("featureTargetDate")
-        featureProductArea = request.form.get("featureProductArea")
-
-        feature = Feature(title=featureTitle, description=featureDescription,
-                          client=featureClient, priority=featureClientPriority,
-                          targetDate=featureTargetDate, productArea=featureProductArea)
+        featureTargetDate = datetime.strptime(request.form.get("featureTargetDate"), "%Y-%m-%d").date()
+        featureProductArea=request.form.get("featureProductArea")
+        feature=Feature(title = featureTitle, description = featureDescription,
+                          client = featureClient, priority = featureClientPriority,
+                          targetDate = featureTargetDate, productArea = featureProductArea)
         db.session.add(feature)
         db.session.commit()
 
@@ -96,8 +94,8 @@ def index():
         })
         return redirect(url_for('index'))
     return render_template("index.html",
-                           clients=clients,
-                           priorities=sorted(priorities),
-                           productAreas=productAreas,
-                           features=features
+                           clients = clients,
+                           priorities = sorted(priorities),
+                           productAreas = productAreas,
+                           features = features
                            )
--- install.sh
+++ install.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
+# Repo location
+#git clone https://github.com/lucky-singh/wishtree.git && cd wishtree
+
 sudo apt update -y
-sudo apt install -y python nginx supervisor python-pip virtualenv git
+sudo apt install -y python python-pip virtualenv git
 
-sudo rm /etc/nginx/sites-enabled/default
-
-sudo cat <<EOT >> flask_settings
-server {
-        location / {
-                proxy_pass http://127.0.0.1:8000;
-                proxy_set_header Host \$host;
-                proxy_set_header X-Real-IP \$remote_addr;
-        }
-}
-EOT
-
-sudo mv flask_settings /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/flask_settings /etc/nginx/sites-enabled/
-sudo /etc/init.d/nginx restart
-
-git clone https://github.com/lucky-singh/wishtree.git
-cd wishtree
 
 virtualenv venv
 source venv/bin/activate
@@ -29,7 +14,7 @@ pip install -r requirements.txt
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
-export DATABASE_URL="postgres://wishtree:1qw21qw2@pg.c6xznrtkhl6g.us-east-1.rds.amazonaws.com:5432/wishtree"
 
-gunicorn app:app
+python app.py
 
+flask run
