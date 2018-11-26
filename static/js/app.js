77ee8942ad45739747f96d7bee0170b7ca23a589
lucky-singh <luckysingh223@gmail.com>
2018-11-26 13:08:46 +0530

    Add form validation

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,40 @@ function toggleFeatureRequestForm() {
     button.textContent = "Cancel";
   }
 }
+
+Date.prototype.toDateInputValue = function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  return local.toJSON().slice(0, 10);
+};
+
+$(document).ready(function() {
+  $("#featureTitle").blur(function formValidator() {
+    let title = $("#featureTitle").val();
+
+    $(".error").remove();
+
+    if (title.length < 1) {
+      $(this).after('<span class= "error" >Title is required.</span>');
+      $(this).addClass("is-invalid");
+    } else {
+      $(this).removeClass("is-invalid");
+    }
+  });
+
+  $("#featureDescription").blur(function formValidator() {
+    let description = $("#featureDescription").val();
+
+    $(".error").remove();
+
+    if (description.length < 1) {
+      $(this).after('<span class= "error" >Description is required.</span>');
+      $(this).addClass("is-invalid");
+    } else {
+      $(this).removeClass("is-invalid");
+    }
+  });
+
+  // Setting date to todays date as default
+  $("#featureTargetDate").val(new Date().toDateInputValue());
+});
