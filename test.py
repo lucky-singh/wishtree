1fb0029ba36d59e61a508aee30f455b7546d57d2
lucky-singh <luckysingh223@gmail.com>
2018-11-27 18:19:27 +0530

    Add more unit test cases

--- test.py
+++ test.py
@@ -4,11 +4,31 @@ import unittest
 
 class FlaskTestCase(unittest.TestCase):
 
-    # Ensure that flask was set up correctly
-    def test_index(self):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_index_status_code(self):
+        """Root url return status code 200"""
         tester = app.test_client(self)
         response = tester.get('/', content_type="html/text")
         self.assertEqual(response.status_code, 200)
+
+    def test_index_content(self):
+        """Page loads with correct html content"""
+        tester = app.test_client(self)
+        response = tester.get('/', content_type="html/text")
+        self.assertTrue(
+            b'<a class="navbar-brand" href="#">Wish Tree</a>' in response.data)
+
+    def test_index_post_request(self):
+        """After sending post request, new Feature gets added into page"""
+        tester = app.test_client(self)
+        response = tester.post('/', data=dict(featureTitle="Test Title", featureDescription="Test Description", featureClient="Client A",
+                                              featureClientPriority=1, featureTargetDate="9999-12-31", featureProductArea="Policies"), follow_redirects=True)
+        self.assertIn(b'9999-12-31', response.data)
 
 
 if __name__ == "__main__":
